951d8dbcdcd880ee72d8919945d07b31d73ef826
Madeleine Shang <mns_catdogmice@yahoo.ca>
2016-11-12 17:49:16 +0000

    interim commit

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var     gulp = require("gulp"),
+        watch = require("gulp-watch"),
+        
+        //styles vars
+        postcss = require('gulp-postcss'),
+        autopre = require('autoprefixer'),
+        cssvars = require("postcss-simple-vars"),
+        nest = require("postcss-nested"),
+        cssimp = require("postcss-import"),
+        mixins = require("postcss-mixins"),
+        browsync = require("browser-sync").create();
+        //end of styles vars
+        
+        
+gulp.task('default', function(){console.log("yay!")})
+
+
+gulp.task('html', function(){console.log("something html")})
+
+
+gulp.task('styles', function(){
+
+    return gulp.src('./app/styles/styles.css')//gulp.src is asynch, gotta let gulp known when completed.
+    .pipe(postcss([cssimp, mixins, cssvars, autopre, nest]))
+    .on('error', function(errInfo){
+        this.emit('end');
+        console.log(errInfo.toString())
+    })
+    .pipe(gulp.dest('./app/temp/styles')); 
+})
+
+
+gulp.task('cssInject', ['styles'] ,function(){
+  return gulp.src('./app/temp/styles.css')
+    .pipe(browsync.stream());
+})
+
+
+gulp.task('watch', function(){
+
+    browsync.init({
+    notify: false,
+// You can use wildcards in here.
+    server: {
+            baseDir: 'app'
+        },
+// We can pick port 8081 or 8082, if you are more of a 2's kind of guy, go for the 8082. Highly recommended.
+    port: 8082
+    });
+    watch('./app/index.html', function(){browsync.reload()})
+    watch('./app/**/*.css', function(){gulp.start('cssInject')}) //styles/
+
+})
+
